ddf4e020187e16607f9a75aca76b7865895f35c8
Katherine-Kumar <Katherine2bk@gmail.com>
2020-05-25 17:15:29 -0700

    finished styling registration form

--- devdesk/src/components/Login Form/Registration.js
+++ devdesk/src/components/Login Form/Registration.js
@@ -37,16 +37,16 @@ export default function Registration() {
         
         <Register id="survey-form" onSubmit={submitForm}>
           <div className="form-option">
-          <Label htmlFor="name" id="name-label">Name: </Label>
-          <input type="text" class="form" id="name" placeholder="Enter your name" name="name" onChange={handleChange} value={Input.name} required/>
+          <Label htmlFor="name" className="active" id="name-label">Name: </Label>
+          <input type="text" className="form" id="name" placeholder="Enter your name" name="name" onChange={handleChange} value={Input.name} required/>
           </div>
           <div className="form-option">
-          <Label htmlFor="email" id="email-label">Email: </Label>
-          <input type="email" class="form" id="email" placeholder="Enter your email" name="email" onChange={handleChange} value={Input.email} required/>
+          <Label htmlFor="email" className="active" id="email-label">Email: </Label>
+          <input type="email" className="form" id="email" placeholder="Enter your email" name="email" onChange={handleChange} value={Input.email} required/>
           </div>
           <div className="form-option">
-          <Label htmlFor="password" id="name-label">Password: </Label>
-          <input type="password" class="form" id="password" placeholder="Enter your name" name="password" onChange={handleChange} value={Input.password} required/>
+          <Label htmlFor="password" className="active" id="name-label">Password: </Label>
+          <input type="password" className="form" id="password" placeholder="Enter your password" name="password" onChange={handleChange} value={Input.password} required/>
           </div>
           <div className="form-option">
           <p>Which role are you currently applying?</p>
