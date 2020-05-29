eb2b9fb3909320ccc4f7714545415d9d279a8c25
Katherine-Kumar <Katherine2bk@gmail.com>
2020-05-28 18:14:08 -0700

    added styling changes on new branch

--- devdesk/src/components/Login Form/Helper-Login.js
+++ devdesk/src/components/Login Form/Helper-Login.js
@@ -1,20 +1,7 @@
 import React, {useState} from "react";
-import "./Helper.css";
-import {useHistory} from "react-router-dom";
+import "./Login.css";
+import {useHistory, Link} from "react-router-dom";
 import styled from "styled-components";
-import { Button } from 'reactstrap';
-
-const Wrapper = styled.form `
-background: #A0E0BB;
-border: 1px solid #777;
-padding: 40px 60px;
-padding-bottom: 10px;
-max-width: 45%;
-margin: 30px auto;
-border-radius: 2%;
-margin-bottom: 6%;
-box-shadow: 1px 10px 21px -7px rgba(48,47,42,1);
-`
 
 export default function HelperForm() {
     const [Input, setInput] = useState({ email: '', password: ''});
@@ -28,21 +15,32 @@ export default function HelperForm() {
         event.preventDefault();
     }
     return(
-        <div className="form-box">
-        <p className="motto">Welcome Helper!</p>
-        <Button color="primary" type="button" id="signup" className="submit-btn" onClick={() => history.push("/registration")}>Sign Up?</Button>
-        <Wrapper id="login-form" onSubmit={submitForm}>
-            <div className="form-option">
-        <label className="active" htmlFor="email" id="email-label">Email: </label>
-        <input type="email" onChange={handleChange} className="form" placeholder="What's your email?" value={Input.email} name="email" required/>
-        </div>
-        <div className="form-option">
-            <label className="active" htmlFor="password" id="password-label">Password: </label>
-        <input type="password" onChange={handleChange} className="form" placeholder="What's your password?" value={Input.password} name="password" required/>
-         </div>
-         <button type="submit" id="create" className="submit-btn">Login</button>
-        </Wrapper>
-    </div>
-
-    )
-}
+        <>
+            <h1>DevDesk.</h1>
+            <p className="motto">Welcome Helper!</p>
+            <div className="form-container">
+                <div id="wrapper">
+                <span style={{color: "Dodgerblue", paddingBottom: "10px"}}>
+                <i class="fas fa-laptop fa-3x"></i>
+                </span>
+                <h3>Please Login</h3>
+                <form onSubmit={submitForm}>
+                    <div className="form-option">
+                <label className="active" htmlFor="email"><span>Username:{""}</span></label>
+                <input type="email" onChange={handleChange} placeholder="What's your username?" value={Input.username} name="username" required/>
+                </div>
+                <div className="form-option">
+                    <label className="active" htmlFor="password"><span>Password:{""}</span></label>
+                <input type="password" onChange={handleChange} placeholder="What's your password?" value={Input.password} name="password" required/>
+                 </div>
+                 <button className="login-btn" type="submit">Login</button>
+                </form>
+                <div className="signup">
+                    <p>Not a member? <Link activeClassName="selected" to="/registration">Sign up for free!</Link></p>
+                </div>
+                </div>
+              <button className="end-motto" onClick={() => history.push("/helper")}> <p>Not a student? Click here.</p></button>
+            </div>
+            </>
+        )
+            }
--- devdesk/src/components/Login Form/Login.js
+++ devdesk/src/components/Login Form/Login.js
@@ -1,20 +1,8 @@
 import React, { useState } from "react";
-import "./Login.css";
-import { useHistory } from "react-router-dom";
+import "./Login.css"
+import { useHistory, Link } from "react-router-dom";
 import styled from "styled-components";
 import axios from "axios";
-
-const WrapperForm = styled.form`
-  background: #ff6332;
-  padding: 40px 60px;
-  border: 1px solid #777;
-  padding-bottom: 10px;
-  max-width: 45%;
-  margin: 30px auto;
-  border-radius: 2%;
-  margin-bottom: 6%;
-  box-shadow: 1px 10px 21px -7px rgba(48, 47, 42, 1);
-`;
 
 export default function Form() {
   const [Input, setInput] = useState({ username: "", password: "" });
@@ -44,55 +32,32 @@ export default function Form() {
   };
 
   return (
-    <>
-      <h1>DevDesk.</h1>
-      <div className="form-container">
-        <p className="motto">We're here to help.</p>
-        <div className="center">
-          <button
-            type="button"
-            id="signup"
-            className="submit-btn"
-            onClick={() => history.push("/registration")}
-          >
-            Sign Up for Free
-          </button>
+<>
+    <h1>DevDesk.</h1>
+    <p className="motto">We're here to help.</p>
+    <div className="form-container">
+        <div id="wrapper">
+        <span style={{color: "Dodgerblue", paddingBottom: "10px"}}>
+        <i class="fas fa-laptop fa-3x"></i>
+        </span>
+        <h3>Please Login</h3>
+        <form onSubmit={submitForm}>
+            <div className="form-option">
+        <label className="active" htmlFor="email"><span>Username:{""}</span></label>
+        <input type="email" onChange={handleChange} placeholder="What's your username?" value={Input.username} name="username" required/>
         </div>
-        <WrapperForm id="login-form" onSubmit={submitForm}>
-          <div className="button-option"></div>
-          <div className="form-option">
-            <label className="active" htmlFor="email" id="email-label">
-              Email:{" "}
-            </label>
-            <input
-              type="text"
-              onChange={handleChange}
-              className="active"
-              placeholder="What's your username?"
-              value={Input.username}
-              name="username"
-              required
-            />
-          </div>
-          <div className="form-option">
-            <label className="active" htmlFor="password" id="password-label">
-              Password:{" "}
-            </label>
-            <input
-              type="password"
-              onChange={handleChange}
-              className="active"
-              placeholder="What's your password?"
-              value={Input.password}
-              name="password"
-              required
-            />
-          </div>
-          <button type="submit" id="create" className="submit-btn">
-            Login
-          </button>
-        </WrapperForm>
-      </div>
+        <div className="form-option">
+            <label className="active" htmlFor="password"><span>Password:{""}</span></label>
+        <input type="password" onChange={handleChange} placeholder="What's your password?" value={Input.password} name="password" required/>
+         </div>
+         <button className="login-btn" type="submit">Login</button>
+        </form>
+        <div className="signup">
+            <p>Not a member? <Link activeClassName="selected" to="/registration">Sign up for free!</Link></p>
+        </div>
+        </div>
+      <button className="end-motto" onClick={() => history.push("/helper")}> <p>Not a student? Click here.</p></button>
+    </div>
     </>
-  );
-}
+)
+    }
